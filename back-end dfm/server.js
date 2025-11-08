90d4ba03c0c6b0c83fa9ef264875451e944fbb97
Yann - DevSphere <137959265+Yannytb@users.noreply.github.com>
2025-11-08 17:04:11 +0100

    Update allowedOrigins to comment out local URLs
    Comment out local development origins in CORS settings.

--- back-end dfm/server.js
+++ back-end dfm/server.js
@@ -28,9 +28,9 @@ connectDB();
 
 // Middleware
 const allowedOrigins = [
-  'http://127.0.0.1:5500',
-  'http://localhost:3000',
-  'http://localhost:5500',
+  //'http://127.0.0.1:5500',
+  //'http://localhost:3000',
+  //'http://localhost:5500',
   'http://localhost:5000'
 ];
 
